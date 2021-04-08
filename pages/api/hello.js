3bb364ec4fffc458e74580c73f266542f1cb7762
rasel-code-dev <raselmr005@gmail.com>
2021-04-08 08:39:34 -0700

    added cors

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,3 +1,20 @@
-export default (req, res) => {
-  res.status(200).json({ text: 'Hello' })
+import moddleware from "../../lib/middleware"
+
+async function handler(req, res) {
+    
+  // res.header('Access-Control-Allow-Origin', '*')
+  // res.header(
+  //   'Access-Control-Allow-Headers',
+  //   'Origin, X-Requested-With, Content-Type, Accept'
+  // )
+
+  // res.statusCode = 200;
+  // res.setHeader("Content-Type", "application/json");
+  
+  await moddleware(req, res)
+
+
+  res.json({ message: 'hello world' })
 }
+
+export default handler
