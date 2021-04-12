29cfa15e5c3e8d974f1f85c759d8b6257e9f88f7
rasel-code-dev <raselmr005@gmail.com>
2021-04-12 11:39:28 -0700

    add redux and material ui

--- next.config.js
+++ next.config.js
@@ -4,5 +4,12 @@ module.exports = (phase, { defaultConfig }) => {
   return {
     /* config options here */
     // useFileSystemPublicRoutes: false,
+    typescript: {
+      // !! WARN !!
+      // Dangerously allow production builds to successfully complete even if
+      // your project has type errors.
+      // !! WARN !!
+      ignoreBuildErrors: true,
+    },
   }
 }
