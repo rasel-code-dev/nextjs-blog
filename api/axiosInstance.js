0bb03f2168a99b80beff0b278498b539f02d946d
rasel-code-dev <raselmr005@gmail.com>
2021-05-26 09:54:34 +0600

    next dummy blog

--- api/axiosInstance.js
+++ api/axiosInstance.js
@@ -1,6 +1,7 @@
 const axios = require("axios");
 
-export const backend = "http://localhost:3000"
+// export const backend = "http://localhost:3000"
+export const backend = "https://nextjs-blog-pj4avuyi0-rasel-code-dev.vercel.app"
 
 
 module.exports = axios.create({
--- pages/api/products.js
+++ pages/api/products.js
@@ -1,16 +1,20 @@
 import * as fs from "fs";
+import path from "path";
+
+const postsDirectory = path.join(process.cwd(), 'data')
 
 export default (req, res, next)=>{
   console.log("api call");
-  // fs.readFile("data/data.json", "utf-8", (err, text)=>{
-  //   let p = JSON.parse(text)
-  //   return res.json({ products: p })
-  // })
+  const fullPath = path.join(postsDirectory, "data.json")
+  fs.readFile(fullPath, "utf-8", (err, text)=>{
+    let p = JSON.parse(text)
+    return res.json({ products: p })
+  })
  
-    return res.json({ products: [
-      {name: "T-Shart", price: 232, qty: 12, image: "static/productsPhoto/gsmarena_010 (2).jpg"},
-      {name: "Monitor", price: 232, qty: 12, image: "static/productsPhoto/gsmarena_016.jpg"},
-      {name: "Mobile", price: 1232, qty: 12, image: "static/productsPhoto/samsung-galaxy-a51-sm-a515-1.jpg"}
-      ] })
+    // return res.json({ products: [
+    //   {name: "T-Shart", price: 232, qty: 12, image: "static/productsPhoto/gsmarena_010 (2).jpg"},
+    //   {name: "Monitor", price: 232, qty: 12, image: "static/productsPhoto/gsmarena_016.jpg"},
+    //   {name: "Mobile", price: 1232, qty: 12, image: "static/productsPhoto/samsung-galaxy-a51-sm-a515-1.jpg"}
+    //   ] })
 
 }
